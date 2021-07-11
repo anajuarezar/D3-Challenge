1431cbc04651a90dd3b77cde7b16fe7f455df06b
anajuarezar <ana.juarezar@gmail.com>
2021-07-11 15:38:22 -0700

    Importing Data

--- assets/js/app.js
+++ assets/js/app.js
@@ -21,4 +21,12 @@ var svg = d3
   .attr("width", svgWidth)
   .attr("height", svgHeight);
 
-  
+
+var chartGroup = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+
+  d3.csv("data.csv").then(function(CensusData) {
+
+
+  })
